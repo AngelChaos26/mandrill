f5da615ae145c2e03a71e81a7b8951f3daa5ab60
Angel Valtierra <angelchaos_26@hotmail.com>
2018-05-07 18:34:50 +0000

    Ne wimplementation for the mandrill email, new templates

--- app/mailers/spree/user_mailer_mandrill.rb
+++ app/mailers/spree/user_mailer_mandrill.rb
@@ -1,13 +1,10 @@
 module Spree
-
   require "mandrill"
-
   class UserMailerMandrill < BaseMailer
-
-
     def reset_password_instructions(user, token, *args)
       @store = Spree::Store.default
       @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: @store.url)
+      current_date = Date.current
       
       if user.isTransfer.nil?
          @edit_password_reset_url += "&is_trans=0"
@@ -16,11 +13,14 @@ module Spree
       end
       
       merge_vars = {
-        "FIRST_NAME" => user.first_name,
-        "USER_URL" =>  @edit_password_reset_url
+        "USER_NAME" => user.first_name.to_s.capitalize,
+        "DATE" => current_date.day.to_s + "/" + current_date.month.to_s + "/" + current_date.year.to_s,
+        "EMAIL" => user.email,
+        "USER_URL" =>  @edit_password_reset_url,
+        "LOCALE" => user.default_language.to_s.downcase
       }
-
-      body = mandrill_template("reset_user_password", merge_vars)
+      
+      body = mandrill_template("change-settings-password-reset", merge_vars) #reset_user_password old template
 
       mail to: user.email, body: body,content_type: "text/html", from:"Wedjourney <contact@wedjourney.com>", subject: "#{@store.name} #{I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions])}"
     end
@@ -30,19 +30,17 @@ module Spree
       @confirmation_url = spree.spree_user_confirmation_url(confirmation_token: token, host: @store.url)
 
       merge_vars = {
-        "FIRST_NAME" => user.first_name,
         "USER_URL" => @confirmation_url,
+        "LOCALE" => user.default_language.to_s.downcase
       }
 
-      body = mandrill_template("confirm_user", merge_vars)
+      body = mandrill_template("account-creation", merge_vars) #confirm_user old template
 
       mail to: user.email, body: body, content_type: "text/html", from:"Wedjourney <contact@wedjourney.com>", subject: "#{@store.name} #{I18n.t(:subject, scope: [:devise, :mailer, :confirmation_instructions])}"
     end
 
 
     private 
-
-
     def mandrill_template(template_name, attributes)
       mandrill = Mandrill::API.new(ENV["SMTP_PASSWORD"])
 
